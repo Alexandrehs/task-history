d2b98784604bb28caf3e243feb8d7b43c7b0aad7
Alexandrehs <tqi_ahenrique@uolinc.com>
2023-12-21 13:43:47 -0300

    initial commit

---
+++ src/main/java/com/personal/taskhistory/TaskHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.personal.taskhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/taskhistory/TaskHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.taskhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
